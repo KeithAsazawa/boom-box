aadc18d94f72911ed6645e59612a5adffd33f25e
KeithAsazawa <keith.asazawa@gmail.com>
2013-07-30 17:03:52 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoomBox::Application.config.secret_token = 'e6f7d01d33914309db3129de7f63bbd1e9562f9298ddeef3aa3b1a416ec1db1a4f1f7a0174ee525e39ef68a8f7b15397f833c417c602f57a23f7b06eaa69a9e1'
